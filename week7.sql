3ef0ef88c39f4a7010513e92800a319d044b893e
JamesVanWaza <jgacuca@gmail.com>
2022-11-12 11:14:49 -0500

    Week7.SQL

---
+++ week7.sql
@@ -0,0 +1,33 @@
+/** Inserting new data */
+INSERT INTO Items
+  SELECT 'Motorola', 'Mobile', 1000 , 1500 , 5000 , 100000 , 100000
+  UNION 
+  SELECT 'Box', 'Steel', 10 , 20 , 200 , 400 , 5000
+  UNION
+  SELECT 'Dell', 'Laptop', 1500 , 3000 , 50000 , 200000 , 1000000
+  UNION
+  SELECT 'Calvin Klein', 'Wallet', 15 , 30 , 7000 , 2000 , 5000
+  UNION
+  SELECT 'Fossil', 'Watch', 50 , 150 , 2000 , 5000 , 10000
+  UNION
+  SELECT 'Sony', 'Earphones', 40 , 100 , 1000 , 7000 , 15000
+  UNION
+  SELECT 'Dell', 'Mouse', 20 , 60 , 1000 , 5000 , 14000
+  UNION
+  SELECT 'Apple', 'Charger', 30 , 50 , 1200 , 4000 , 30000
+  UNION
+  SELECT 'Aldo', 'Footwear', 35 , 75 , 1200 , 6200 , 23000
+  UNION
+  SELECT 'Hollister', 'CasualShirt', 40 , 100 , 1500 , 3000 , 45000
+  UNION
+  SELECT 'Global', 'Fan', 20 , 50 , 150 , 5000 , 30000
+  UNION
+  SELECT 'Samsung', 'TV', 500 , 1700 , 5000 , 15000 , 754200
+  UNION
+  SELECT 'Body Shop', 'Perfumes', 5 , 12 , 1200 , 5200 , 23000
+  UNION
+  SELECT 'Under Armour', 'Shoes', 50 , 170 , 3000 , 25000 , 250000
+  UNION
+  SELECT 'HP', 'Printer', 250 , 650 , 7000 , 35000 , 850000
+  
+  
